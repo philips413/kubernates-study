fbb24f4dee101e37e55b1da8c3de3700ff1fe78f
Philips413 <philips413@kakao.com>
2024-07-10 23:00:09 +0900

    bookmarker-api 다시 세팅

---
+++ bookmarker-api/src/main/java/psnl/marker/api/BookmarkController.java
@@ -0,0 +1,39 @@
+package psnl.marker.api;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import psnl.marker.domain.BookmarkDTO;
+import psnl.marker.domain.BookmarkService;
+import psnl.marker.domain.BookmarksDTO;
+import psnl.marker.domain.CreateBookmarkRequest;
+
+@RestController
+@RequestMapping("/api/bookmarks")
+@RequiredArgsConstructor
+public class BookmarkController {
+    private final BookmarkService bookmarkService;
+
+    @GetMapping
+    public BookmarksDTO getBookmarks(@RequestParam(defaultValue = "1") Integer page,
+                                     @RequestParam(defaultValue = "") String query) {
+        if(query == null || query.trim().length() == 0) {
+            return bookmarkService.getBookmarks(page);
+        }
+        return bookmarkService.searchBookmarks(query, page);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public BookmarkDTO createBookmark(@RequestBody @Valid CreateBookmarkRequest request) {
+        return bookmarkService.createBookmark(request);
+    }
+}
